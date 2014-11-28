47672a6b7980aa1f6a37627d9ed4dbdce5984ab9
Daniel Stiner <danstiner@gmail.com>
2014-11-28 14:08:28 -0800

    Support for more commands and basic expressions
    Statements: GOTO, :Label, VER, VERIFY
    Expressions: TRUE, FALSE, EXIST [FILEPATH], NOT [EXPR]

--- src/BatchParser.hs
+++ src/BatchParser.hs
@@ -1,7 +1,10 @@
 module BatchParser (
     parse
+  , statement
+  , expression
   , Script
   , Statement (..)
+  , Expression (..)
 ) where
 
 import           Control.Applicative
@@ -9,19 +12,37 @@ import           Control.Monad          (void)
 import           Control.Monad.Identity (Identity)
 import           Data.Char
 import           Data.List              (intercalate)
-import           Text.Parsec            (ParseError, Parsec, (<?>))
+import           Text.Parsec            (ParseError, Parsec)
 import qualified Text.Parsec            as Parsec
 import           Text.Parsec.Char
 import           Text.Parsec.Language
 import           Text.Parsec.Token
 
+type LabelName = String
 type Script = [Statement]
+data Expression =
+    NotExpr Expression
+  | TrueExpr
+  | FalseExpr
+  | Exist FilePath
+  deriving (Eq, Show)
+
 data Statement =
     EchoMessage String
   | EchoEnabled Bool
+  | Find String [FilePath]
+  | Goto LabelName
+  | GotoEof
+  | If Expression Statement Statement
+  | Label LabelName
   | Quieted Statement
   | Rem String
+  | Rename FilePath FilePath
   | RmDir { rmDirRecurse :: Bool, rmDirQuiet :: Bool, rmDirPath :: FilePath }
+  | Type [FilePath]
+  | Ver
+  | Verify Bool
+  | Noop
   deriving (Eq, Show)
 
 parse :: String -> Either ParseError Script
@@ -29,35 +50,60 @@ parse = Parsec.parse script "(source)"
 
 script :: Parsec String u [Statement]
 script = do
-  lines <- many line
+  s <- statements
   Parsec.eof
-  return (flatten lines)
-  where
-    flatten = intercalate []
-
-line :: Parsec String st [Statement]
-line = do
-  statements <- statements
-  terminateLine
-  return statements
+  return s
 
 statements :: Parsec String st [Statement]
-statements = do
-  s <- statement
-  remaining <- statements <|> return []
-  return (s:remaining)
+statements = ws *> (empty <|> content) where
+  ws = whiteSpace tokenParser
+  empty = Parsec.eof *> return []
+  content = do
+    s <- statement
+    remaining <- statements <|> return []
+    return (s:remaining)
 
 statement :: Parsec String st Statement
-statement = foldr (<|>) (Parsec.unexpected "no command matched") commands
+statement = foldr
+  ((<|>) . followedByWhiteSpace)
+  (Parsec.unexpected "no command matched")
+  commands
+  where
+    followedByWhiteSpace p = do
+      r <- p
+      whiteSpace tokenParser
+      return r
 
 commands =
   [
     echo
+  , goto
+  , gotoEof
+  , ifStatement
+  , label
   , quieted
   , rd
   , remStatement
   , rmdir
+  , ver
   ]
+
+expression :: Parsec String st Expression
+expression = trueExpr <|> falseExpr <|> existExpr <|> notExpr
+
+trueExpr :: Parsec String st Expression
+trueExpr = string "TRUE" *> return TrueExpr
+
+falseExpr :: Parsec String st Expression
+falseExpr = string "FALSE" *> return FalseExpr
+
+existExpr :: Parsec String st Expression
+existExpr = fmap
+  Exist
+  (string "EXIST" >> Parsec.skipMany1 printableWhitespace >> Parsec.manyTill Parsec.anyChar terminateExpr)
+
+notExpr :: Parsec String st Expression
+notExpr = fmap NotExpr (string "NOT" >> Parsec.skipMany1 printableWhitespace >> expression)
 
 echo :: Parsec String st Statement
 echo = string "ECHO" >> (echodot <|> echonormal)
@@ -77,6 +123,15 @@ remStatement = fmap Rem
   >> Parsec.skipMany1 printableWhitespace
   >> stringExp)
 
+label :: Parsec String st Statement
+label = fmap Label (char ':' >> stringExp)
+
+goto :: Parsec String st Statement
+goto = fmap Goto (string "GOTO" >> Parsec.skipMany1 printableWhitespace >> stringExp)
+
+gotoEof :: Parsec String st Statement
+gotoEof = string "GOTO:eof" >> return GotoEof
+
 rd :: Parsec String st Statement
 rd = fmap (RmDir False False) (string "RD" >> stringExp)
 
@@ -85,6 +140,26 @@ rmdir = fmap (RmDir False False) (string "RMDIR" >> stringExp)
 
 quieted :: Parsec String st Statement
 quieted = fmap Quieted (char '@' >> statement)
+
+ver :: Parsec String st Statement
+ver = string "VER" >> (verStatement <|> verifyStatement)
+
+verStatement :: Parsec String st Statement
+verStatement = terminateStatement >> return Ver
+
+verifyStatement :: Parsec String st Statement
+verifyStatement = string "IFY" >> Parsec.skipMany1 printableWhitespace >> (t <|> f) where
+  t = trueExpr >> return (Verify True)
+  f = falseExpr >> return (Verify False)
+
+ifStatement :: Parsec String st Statement
+ifStatement = do
+  string "IF"
+  Parsec.skipMany1 printableWhitespace
+  exp <- expression
+  Parsec.skipMany1 printableWhitespace
+  consequent <- statement
+  return (If exp consequent Noop)
 
 stringExp = Parsec.manyTill Parsec.anyChar terminateStatement
 
@@ -95,3 +170,22 @@ terminateLine = void endOfLine <|> Parsec.eof
 
 terminateStatement :: Parsec String u ()
 terminateStatement = terminateLine
+
+terminateExpr :: Parsec String u ()
+terminateExpr = void Parsec.space <|> Parsec.eof
+
+tokenParser :: TokenParser st
+tokenParser = makeTokenParser languageDef
+
+languageDef :: LanguageDef st
+languageDef = emptyDef
+    { commentStart   = ""
+    , commentEnd   = ""
+    , commentLine  = "::"
+    , nestedComments = True
+    , identStart   = letter
+    , identLetter  = alphaNum <|> oneOf "_'"
+    , reservedNames  = []
+    , reservedOpNames= []
+    , caseSensitive  = False
+    }
--- test/BatchParserTests.hs
+++ test/BatchParserTests.hs
@@ -6,7 +6,8 @@ import           BatchParser
 
 import           Data.Char
 import           Data.List
-import           Text.ParserCombinators.Parsec
+import           Text.Parsec            (ParseError, Parsec)
+import qualified Text.Parsec            as Parsec
 
 import           Test.Framework
 import           Test.Framework.Providers.QuickCheck2 (testProperty)
@@ -16,45 +17,104 @@ import           Test.QuickCheck.Property             as Property
 tests :: [Test]
 tests = [
           testProperty "<empty>" prop_parseEmpty
+        , testProperty "<whitespace>" prop_parseWhitespace
         , testProperty "ECHO ON" prop_parseEchoOn
         , testProperty "ECHO OFF" prop_parseEchoOff
         , testProperty "@ECHO OFF" prop_parseAtEchoOff
         , testProperty "ECHO [message]" prop_parseEchoMessage
         , testProperty "ECHO [message]\\nECHO [message]" prop_parseEchoMessageTwice
+        , testProperty "IF [EXPR] ECHO Hello" prop_parseIf
+        , testProperty "GOTO [label]" prop_statement_goto
+        , testProperty ":[label]" prop_statement_label
+        , testProperty "VER" prop_statement_ver
+        , testProperty "VERIFY [bool]" prop_statement_verify
+        , testProperty "TRUE" prop_exp_true
+        , testProperty "FALSE" prop_exp_false
+        , testProperty "EXIST [FILEPATH]" prop_exp_exist
+        , testProperty "NOT TRUE" prop_exp_nottrue
         ]
 
 prop_parseEmpty :: Property.Result
-prop_parseEmpty = assertParse "" []
+prop_parseEmpty = assertParseScript "" []
+
+prop_parseWhitespace :: Property
+prop_parseWhitespace =
+  forAll genWhitespace $ \whitespace ->
+  assertParseScript whitespace []
+  where
+    genWhitespace = suchThat arbitrary (all isSpace)
 
 prop_parseEchoOn :: Property
 prop_parseEchoOn =
   forAll (casing "ON") $ \arg ->
-  assertParse ("ECHO " ++ arg) [EchoEnabled True]
+  assertParseStatement ("ECHO " ++ arg) (EchoEnabled True)
 
 prop_parseEchoOff :: Property
 prop_parseEchoOff =
   forAll (casing "OFF") $ \arg ->
-  assertParse ("ECHO " ++ arg) [EchoEnabled False]
+  assertParseStatement ("ECHO " ++ arg) (EchoEnabled False)
 
 prop_parseAtEchoOff :: Property
 prop_parseAtEchoOff =
   forAll (casing "OFF") $ \arg ->
-  assertParse ("@ECHO " ++ arg) [Quieted (EchoEnabled False)]
+  assertParseStatement ("@ECHO " ++ arg) (Quieted (EchoEnabled False))
 
 prop_parseEchoMessage :: Property
 prop_parseEchoMessage =
   forAll messageString $ \msg ->
-  assertParse ("ECHO " ++ msg) [EchoMessage msg]
+  assertParseStatement ("ECHO " ++ msg) (EchoMessage msg)
 
 prop_parseEchoMessageTwice :: Property
 prop_parseEchoMessageTwice =
   forAll messageString $ \msg1 ->
   forAll messageString $ \msg2 ->
-  assertParse ("ECHO " ++ msg1 ++ "\nECHO " ++ msg2) [EchoMessage msg1,EchoMessage msg2]
+  assertParseScript ("ECHO " ++ msg1 ++ "\nECHO " ++ msg2) [EchoMessage msg1,EchoMessage msg2]
 
-assertParse :: String -> [Statement] -> Property.Result
-assertParse script expected =
+prop_parseIf :: Property.Result
+prop_parseIf = assertParseStatement
+  "IF TRUE ECHO hello"
+  (If TrueExpr (EchoMessage "hello") Noop)
+
+prop_statement_goto :: Property.Result
+prop_statement_goto = assertParseStatement "GOTO Label" (Goto "Label")
+
+prop_statement_label :: Property.Result
+prop_statement_label = assertParseStatement ":Label" (Label "Label")
+
+prop_statement_ver :: Property.Result
+prop_statement_ver = assertParseStatement "VER" Ver
+
+prop_statement_verify :: Bool -> Property.Result
+prop_statement_verify b =
+  assertParseStatement ("VERIFY " ++ map toUpper (show b)) (Verify b)
+
+prop_exp_true :: Property.Result
+prop_exp_true = assertParseExpression "TRUE" TrueExpr
+
+prop_exp_false :: Property.Result
+prop_exp_false = assertParseExpression "FALSE" FalseExpr
+
+prop_exp_exist :: Property.Result
+prop_exp_exist = assertParseExpression "EXIST /dev/null" (Exist "/dev/null")
+
+prop_exp_nottrue :: Property.Result
+prop_exp_nottrue = assertParseExpression "NOT TRUE" (NotExpr TrueExpr)
+
+assertParseScript :: String -> [Statement] -> Property.Result
+assertParseScript script expected =
   case BatchParser.parse script of
+    Left error -> mkResult False (show error)
+    Right parsed -> mkResult (parsed == expected) (show parsed ++ "/=" ++ show expected)
+
+assertParseStatement :: String -> Statement -> Property.Result
+assertParseStatement = assertParse BatchParser.statement
+
+assertParseExpression :: String -> Expression -> Property.Result
+assertParseExpression = assertParse BatchParser.expression
+
+assertParse :: (Show a, Eq a) => Parsec String () a -> String -> a -> Property.Result
+assertParse parser source expected =
+  case Parsec.parse parser "(source)" source of
     Left error -> mkResult False (show error)
     Right parsed -> mkResult (parsed == expected) (show parsed ++ "/=" ++ show expected)
 
