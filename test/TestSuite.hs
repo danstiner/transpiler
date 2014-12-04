1f4d9b0a96691b87adf10e9e30dd5d3cf25c3723
Daniel Stiner <danstiner@gmail.com>
2014-12-03 23:47:12 -0800

    Add tests to enforce code formatting

--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -6,15 +6,20 @@ import qualified Batch.TransformerTests
 import qualified CSharp.PrinterTests
 import qualified CSharp.TransformerTests
 
-import           Test.Framework   (defaultMain, testGroup)
+import           Language.Haskell.Format.Tests  as Hfmt
+import           Test.Framework                 (defaultMain, testGroup)
+import           Test.Framework.Providers.HUnit
 
 main :: IO ()
-main = defaultMain tests where
-  tests =
-    [
+main = do
+  src <- hunitGlob "src/**/*.hs"
+  test <- hunitGlob "test/**/*.hs"
+  defaultMain [
       testGroup "Batch.Lexer" Batch.LexerTests.tests
     , testGroup "Batch.Parser" Batch.ParserTests.tests
     , testGroup "Batch.Transformer" Batch.TransformerTests.tests
     , testGroup "CSharp.Printer" CSharp.PrinterTests.tests
     , testGroup "CSharp.Transformer" CSharp.TransformerTests.tests
+    , testGroup "Validate Formatting in src/**/*.hs" (hUnitTestToTests src)
+    , testGroup "Validate Formatting in test/**/*.hs" (hUnitTestToTests test)
     ]
