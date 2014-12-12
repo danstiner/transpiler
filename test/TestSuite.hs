20de634c0a8e3024f45f6174e849966e59f80fda
Daniel Stiner <danstiner@gmail.com>
2014-12-11 22:35:34 -0800

    Switch to package-based Hfmt tests

--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -11,15 +11,11 @@ import           Test.Framework                 (defaultMain, testGroup)
 import           Test.Framework.Providers.HUnit
 
 main :: IO ()
-main = do
-  src <- hunitGlob "src/**/*.hs"
-  test <- hunitGlob "test/**/*.hs"
-  defaultMain [
+main = defaultMain [
       testGroup "Batch.Lexer" Batch.LexerTests.tests
     , testGroup "Batch.Parser" Batch.ParserTests.tests
     , testGroup "Batch.Transformer" Batch.TransformerTests.tests
     , testGroup "CSharp.Printer" CSharp.PrinterTests.tests
     , testGroup "CSharp.Transformer" CSharp.TransformerTests.tests
-    , testGroup "Validate Formatting in src/**/*.hs" (hUnitTestToTests src)
-    , testGroup "Validate Formatting in test/**/*.hs" (hUnitTestToTests test)
+    , testGroup "Source formatting" (hUnitTestToTests $ Hfmt.hunitPackage "transpiler.cabal")
     ]
